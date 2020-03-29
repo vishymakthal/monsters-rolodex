f8e2b7108406909c2f531593299887ad493d3669
Vishy Makthal <vmakthal@cisco.com>
2020-03-29 18:25:42 -0400

    Updates

---
+++ precache-manifest.3d695876dcb0e6a1c5104217e8d70690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5539f8b838605fcda6a9ccafb08e62",
+    "url": "/vishymakthal.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ca46b4afaa30c47737d6",
+    "url": "/vishymakthal.github.io/monsters-rolodex/static/css/main.a0d36ae9.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/vishymakthal.github.io/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/vishymakthal.github.io/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca46b4afaa30c47737d6",
+    "url": "/vishymakthal.github.io/monsters-rolodex/static/js/main.d9e5f818.chunk.js"
+  },
+  {
+    "revision": "9bf6e11b3fa1891c7b0c",
+    "url": "/vishymakthal.github.io/monsters-rolodex/static/js/runtime-main.6800d190.js"
+  }
+]);
